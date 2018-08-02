68bf370005441620cdb421bfe0addca1016a2a39
jneilson31 <jeremyneilson31@gmail.com>
2018-08-02 16:12:32 -0600

    RecentFixes

--- scripts/js/aucona.js
+++ scripts/js/aucona.js
@@ -1,5 +1,4 @@
-// Hovering over letters will change the Color
-
+// Change colors on Hover
 $(".changeColor").mouseenter(function () {
 	$(this).css("color", "red");
 })
@@ -8,19 +7,21 @@ $(".changeColor").mouseleave(function () {
 	$(this).css("color", "fff");
 })
 
+//Toggle Hamburger Menu for Secondary Nav
 $(".hamburger").click(function () {
 	console.log("clickedHamburger")
 	$("#hamburgerMenu").toggleClass("navShow");
 });
 
+// Code to change from Active to inactive
 $('.navIcons').click(function () {
 	$('.navIcons').removeClass('active');
 	$(this).addClass('active');
 });
 
+// Fade Out Icons for name replacement
 $('#home').click(function () {
 	$(this).css("opacity", "0");
-
 });
 
 
