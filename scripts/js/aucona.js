c99d6ad9faecf22635fa1453250c965413395c70
Jeremy <jeremyneilson31@gmail.com>
2018-08-18 11:11:02 -0600

    animationSlideChanges

--- scripts/js/aucona.js
+++ scripts/js/aucona.js
@@ -32,3 +32,11 @@ $(function () {
 	}, 3000);
 });
 
+$('.box-wrapper').each(function (index, element) {
+
+	setTimeout(function () {
+		element.classList.remove('loading');
+	}, index * 500);
+
+});
+
